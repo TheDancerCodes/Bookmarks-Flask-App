85f3d267218402d13d35d1434145c6faa5f6d1cd
Roger Taracha <rojtaracha@gmail.com>
2017-01-26 23:23:16 +0300

    HTML Tempplates with Jinja   + We can display the following using Jinja Templates:    * Strings    ** Lists    *** Objects    **** Methods

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -3,11 +3,25 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+class User:
+    """A User Class."""
+
+    def __init__(self, firstname, lastname):
+        """Function that initializes the User Class."""
+        self.firstname = firstname
+        self.lastname =  lastname
+
+    def initials(self):
+        """Function that returns initials of fname and lname."""
+        return "{}. {}.".format(self.firstname[0], self.lastname[0])
+
 @app.route('/')
 @app.route('/index')
 def index():
     """View Function that is returned as a Response for a HTTP Request."""
-    return render_template('index.html')
+    return render_template('index.html', title="Title passed from View to Template.",
+                            text=["first", "second", "third"],
+                            user=User("Roger", "Taracha"))
 
 if __name__ == "__main__":
     app.run()
