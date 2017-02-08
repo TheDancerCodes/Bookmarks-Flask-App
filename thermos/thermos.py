188f792e24aef4bd400f9835bdcc7e6d1b376bd1
Roger Taracha <rojtaracha@gmail.com>
2017-02-08 15:27:11 +0300

    Storing and Retreiving New Bookmarks.   + Added a static method in models.py that returns latest bookmarks sorted by date.   + Imported models in thermos.py to enable saving of bookmarks to DB.

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -5,37 +5,26 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-from forms import BookmarkForm
 
 # Determine path to current python file
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-bookmarks = []
 app.config['SECRET_KEY'] = '9\xbcl\xeb\x83s\xa5]\xf7 +5c\xbd\xafh)\xfcts\xb2Y\x1f\xbd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'birika.db')
 # Initialise SQLAlchemy
 # db variable reps DB connection & provides access to all flask_alchemy functionality
 db = SQLAlchemy(app)
 
-def store_bookmark(url, description):
-    """Function that stores the bookmarks as dicts in a list."""
-    bookmarks.append(dict(
-        url = url,
-        description = description,
-        user = "Taracha",
-        date = datetime.utcnow
-    ))
+from forms import BookmarkForm
+import models
 
-def new_bookmarks(num):
-    """Functon that returns last bookmarks sorted by date."""
-    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
 
 @app.route('/')
 @app.route('/index')
 def index():
     """View Function that is returned as a Response for a HTTP Request."""
-    return render_template('index.html', new_bookmarks=new_bookmarks(5))
+    return render_template('index.html', new_bookmarks=models.Bookmark.latest_bookmarks(5))
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -44,7 +33,9 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        store_bookmark(url, description)
+        bm = models.Bookmark(url=url, description=description)
+        db.session.add(bm)
+        db.session.commit()
         flash("Stored Bookmark '{}'".format(description))
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
