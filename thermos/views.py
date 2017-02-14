330f9089bdca9d0ff064b505b47dc4ebc2fbe523
Roger Taracha <rojtaracha@gmail.com>
2017-02-14 16:30:52 +0300

    Added the Login Page   + Used @login_manager.user_loader to retreive a user object based on the ID.

--- thermos/views.py
+++ thermos/views.py
@@ -1,13 +1,15 @@
-from flask import render_template, redirect, url_for, flash
-from flask_login import login_required
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, login_user
 
-from thermos import app, db
-from forms import BookmarkForm
+from thermos import app, db, login_manager
+from forms import BookmarkForm, LoginForm
 from models import User, Bookmark
 
-# Fake Login [mocks the login process with a default user.]
-def logged_in_user():
-    return User.query.filter_by(username='Taracha').first()
+@login_manager.user_loader
+def load_user(userid):
+    """View Function to retreive a user object based on the ID."""
+    return User.query.get(int(userid))
+
 
 @app.route('/')
 @app.route('/index')
@@ -32,8 +34,23 @@ def add():
 
 @app.route('/user/<username>')
 def user(username):
+    """View Function displaying a users Bookmarks."""
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+@ app.route("/login", methods=["GET", "POST"])
+def login():
+    """View Function that logs users in."""
+    form = LoginForm()
+    if form.validate_on_submit():
+        # login and validate the user
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None:
+            login_user(user, form.remember_me.data)
+            flash("Logged in succesfully as {}.".format(user.username))
+            return redirect(request.args.get('next') or url_for('index'))
+        flash("Incorrect Username or Password.")
+    return render_template("login.html", form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
