b0e5a80aef9012003cc3965ef1de553a49fc854f
Roger Taracha <rojtaracha@gmail.com>
2017-02-15 12:32:45 +0300

    Current_user and Logout.   + import current_user variable and logout_user function   + link bm to user so that it is added to their profile page.   + implement logout_user in logout  View Function that logs out users and redirects them to index page.

--- thermos/views.py
+++ thermos/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_required, login_user
+from flask_login import login_required, login_user, logout_user, current_user
 
 from thermos import app, db, login_manager
 from forms import BookmarkForm, LoginForm
@@ -25,7 +25,7 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        bm = Bookmark(user=logged_in_user(), url=url, description=description)
+        bm = Bookmark(user=current_user, url=url, description=description)
         db.session.add(bm)
         db.session.commit()
         flash("Stored Bookmark '{}'".format(description))
@@ -38,7 +38,7 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
 
-@ app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     """View Function that logs users in."""
     form = LoginForm()
@@ -51,6 +51,12 @@ def login():
             return redirect(request.args.get('next') or url_for('index'))
         flash("Incorrect Username or Password.")
     return render_template("login.html", form=form)
+
+@app.route("/logout")
+def logout():
+    """View Function that logs out users and redirects them to index page."""
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.errorhandler(404)
 def page_not_found(e):
